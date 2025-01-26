47343d93f9396e42e62cd6d6a44eb87a65de78af
sbuendiaf <sbuendiaf@gmail.com>
2025-01-26 15:33:20 +0100

    Normalize line endings to LF

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+    return (
+        <main className="container">
+            <h1>Sobre Nosotros</h1>
+            <p>
+                Deltuvi es una empresa dedicada a la formación sanitaria avanzada, con un enfoque
+                práctico y realista en situaciones de emergencia.
+            </p>
+        </main>
+    );
+}
+
+export default About;
